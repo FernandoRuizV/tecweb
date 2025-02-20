4c956c55548fb30704f88dba524e4610fef7acbe
Fernan <lasleguminosas@gmail.com>
2025-02-20 16:09:42 -0600

    Actividad 3 p08

--- practicas/p08-base/set_producto_v2.php
+++ practicas/p08-base/set_producto_v2.php
@@ -16,6 +16,7 @@ $precio = $_POST['precio'];
 $detalles = $_POST['detalles']; 
 $unidades = $_POST['unidades']; 
 $imagen   = $_POST['imagen']; 
+$eliminado= 0;
 /** Crear una tabla que no devuelve un conjunto de resultados */
 $sql_verificar = "SELECT COUNT(*) AS total FROM productos WHERE nombre = '{$nombre}' AND marca = '{$marca}' AND modelo = '{$modelo}'";
 $resultado = mysqli_query($link, $sql_verificar);
@@ -25,12 +26,13 @@ if ($resultado)
     list($total) = mysqli_fetch_row($resultado);
     if($total>0)
     {
-        echo "SU REGISTRO YA SE ENCUENTRA EN EL SISTEMA";
+        echo "SU REGISTRO YA SE ENCUENTRA EN EL SISTEMA  ". $total."VECES";
     }else{
-        $sql_insertar = "INSERT INTO productos VALUES (null,'{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
-        echo "Registro exitoso";
-        if (mysqli_query($link, $sql_insertar)) {
-            echo "Registro exitoso.";
+        $sql_insertar = "INSERT INTO productos VALUES (null,'{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', {$eliminado})";
+        if (mysqli_query($link, $sql_insertar)) 
+        {
+            echo "Registro exitoso <br>";
+            echo "Valores insertados <br>".$sql_insertar;
         } else {
             echo "Error al registrar el producto: " . mysqli_error($link);
         }
